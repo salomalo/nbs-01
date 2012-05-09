1094a821ddfc609389ebd8e179900663363f38f4
nguyenxuanmui <nguyenxuanmui@067289a7-69ab-d125-2915-3cd7ab555523>
2012-05-09 09:27:37 +0000

    Update com rental (view apartments) & mod rental filter

--- modules/mod_rental_filter/tmpl/default.php
+++ modules/mod_rental_filter/tmpl/default.php
@@ -9,9 +9,52 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<h1>Filter</h1>
-<?php foreach ($list as $item) : ?>
-	<dt><?php echo $item->title;?></dt>
-	<dd><?php echo $item->data;?></dd>
-<?php endforeach; ?>
-</dl>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script>
+	jQuery(function($){
+		$('#location').change(function(){
+			var t = $(this);
+			
+			var link = 'index.php?option=com_rental&view=ajax_location&location=' + t.val();
+
+			
+		});
+		
+		$('#click-to-show').click(function(){
+			if ($('#location').val() == '')
+			{
+				alert('You must select location first');
+				return false;
+			}
+			
+			$('#container-location').show();
+		});
+		
+		$(document).click(function() {
+			$('#container-location').hide();
+		});
+		
+		$("#container-location, #click-to-show").click(function(e) {
+			e.stopPropagation(); // This is the preferred method.
+			return false;
+		});
+	});
+</script>
+
+<div id="searchWrapperOuter" class="curveBottom">
+
+	<select id="location" style="float: left;">
+		<option value="">- Select -</option>
+		<option value="bronx">Bronx</option>
+		<option value="Mylyn">Mylyn</option>
+	</select>
+	
+	<div id="show-location" style="float: left;">
+		<div id="click-to-show" style="border: 1px solid #AAA; width: 95px; padding: 3px; margin-bottom: 1px;">Select Location</div>
+		<div id="container-location" style="float: left; border: 2px solid #AAA; display: none;">
+			
+		</div>
+	</div>
+	
+</div>
