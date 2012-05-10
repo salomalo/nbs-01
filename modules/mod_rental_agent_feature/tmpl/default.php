78adeddd3b81f42324fafebb71a1b16458f9920d
thaihtvn <thaihtvn@067289a7-69ab-d125-2915-3cd7ab555523>
2012-05-10 16:32:49 +0000

    - Install and config all sample module HTML in frontpage

---
+++ modules/mod_rental_agent_feature/tmpl/default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_rental_agent_feature
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div class="clearfix" id="feature">
+  <span class="plug"></span>
+    <h2><a href="/rental-agents" class=" ">Find and work with the top <br> agents in NYC!</a></h2>
+    <p>We've created a Hall of Fame of the best agents on our site.</p>
+    <div class="action">
+    <a class="btnHtml big grey bold" href="/rental-agents">Find your match!</a>
+    </div>
+</div>
