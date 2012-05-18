f6790262a9272fc4b8e6d1556c14e57b46a893bf
thaihtvn@gmail.com <thaihtvn@gmail.com@067289a7-69ab-d125-2915-3cd7ab555523>
2012-05-18 03:50:26 +0000

    - added layout for broker's profile (ex: http://www.nakedapartments.com/a/profile/melissa-gomez/5ls)

---
+++ trunk/components/com_rental/views/broker/tmpl/profile.php
@@ -0,0 +1,100 @@
+<?php
+?>
+
+<div id="broker_profile">
+  <div class="profileMasthead clearfix"> <span class="block" id="name">
+    <h1>Melissa Gomez</h1>
+    <a href="mailto:melissa@anchornyc.com" class=" ">melissa@anchornyc.com</a> <br>
+    (513) 404-7720 
+    | <a target="new" rel="nofollow" href="http://www.anchornyc.com/melissa">My web site</a> </span> <span class="block" id="rating"> <span class="title">Overall Rating:</span> <span class="noRating"></span> </span> <span class="block last" id="brokerage"> <span class="title">Brokerage:</span> Anchor Associates </span> 
+    <!-- <span class="block last">
+    </span> -->
+    <div class="clear"></div>
+  </div>
+  
+  <!-- Begin: Left Col -->
+  <div id="leftCol">
+    <div class="bio clearfix"> <span class="block"> <img src="http://s3.amazonaws.com/nakedapartments/broker/photos/5820/normal.jpg?1337200903" class="borderWhite" alt="Melissa Gomez Photo"> </span>
+      <h2>Meet Melissa</h2>
+      <p><span lang="en-US"><font size="2" face="Calibri,sans-serif"><span style="font-size:11pt;">Melissa grew up in Cincinnati and moved to NYC in 2004 to study Communications at Pace University.&nbsp; During her time at college, she began taking classes at the Upright Citizens Brigade Theatre (UCB) and studied improv &amp; sketch comedy.<br>
+        <br>
+        The leading principles in this type of acting training is actively listening to your scene partner and saying YES &amp; adding to their original idea. I promise to take these principles into our interactions as well. I will actively listen to your needs and say yes to your wants and add some things that you had overlooked or not thought of just yet. I know how nerve-racking finding a home can be and it would be my pleasure to make this process as informative &amp; fun as it can be and have you in the apartment that you love.<br>
+        <br>
+        Let's start a conversation!</span></font></span><br>
+      </p>
+    </div>
+    <div class="clear"></div>
+    <div class="leftMod">
+      <div class="resultsPagination"> </div>
+      <h2>Renter Reviews of Melissa</h2>
+      <span class="messageNoContent">Melissa Gomez has not been reviewed yet</span>
+      <div class="resultsPagination"> </div>
+    </div>
+  </div>
+  <!-- End: Left Col --> 
+  
+  <!-- Begin: Right Col -->
+  <div id="rightCol">
+    <div class="rightMod">
+      <div class="shareButtons"><a target="new" rel="nofollow" class="shareButton facebook" href="http://www.facebook.com/sharer.php?u=http%253A%252F%252Fwww.nakedapartments.com%252Fa%252Fprofile%252Fmelissa-gomez%252F5ls&amp;t=">&nbsp;</a><a target="new" rel="nofollow" class="shareButton twitter" href="http://twitter.com/share?url=http%3A%2F%2Fwww.nakedapartments.com%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls&amp;text=">&nbsp;</a><a target="new" rel="nofollow" class="shareButton linkedin" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http%3A%2F%2Fwww.nakedapartments.com%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls&amp;title=">&nbsp;</a><a rel="nofollow" data-url="/share/emails/new?url=http%3A%2F%2Fwww.nakedapartments.com%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls" class="shareButton email modal cboxElement" href="#">&nbsp;</a><span class="fbLikeButton">
+        <div id="fb-root" class=" fb_reset">
+          <div style="position: absolute; top: -10000px; height: 0pt; width: 0pt;">
+            <div>
+              <iframe id="fb_xdm_frame_http" name="fb_xdm_frame_http" src="http://static.ak.facebook.com/connect/xd_arbiter.php?version=6#channel=fe786de113b5e2&amp;origin=http%3A%2F%2Fwww.nakedapartments.com&amp;channel_path=%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls%3Ffb_xd_fragment%23xd_sig%3Df7cf81e2ad11f%26&amp;transport=postmessage"></iframe>
+              <iframe id="fb_xdm_frame_https" name="fb_xdm_frame_https" src="https://s-static.ak.facebook.com/connect/xd_arbiter.php?version=6#channel=fe786de113b5e2&amp;origin=http%3A%2F%2Fwww.nakedapartments.com&amp;channel_path=%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls%3Ffb_xd_fragment%23xd_sig%3Df7cf81e2ad11f%26&amp;transport=postmessage"></iframe>
+            </div>
+            <div>
+              <iframe scrolling="no" id="f39c86760c68484" name="f158792bd8044da" style="border: medium none; overflow: hidden; height: 240px; width: 575px;" class="FB_UI_Hidden" src="http://www.facebook.com/dialog/oauth?api_key=181832445204475&amp;app_id=181832445204475&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D6%23cb%3Df1d883666dd0fc%26origin%3Dhttp%253A%252F%252Fwww.nakedapartments.com%252Ffe786de113b5e2%26domain%3Dwww.nakedapartments.com%26relation%3Dparent.parent&amp;client_id=181832445204475&amp;display=none&amp;domain=www.nakedapartments.com&amp;locale=en_US&amp;origin=1&amp;redirect_uri=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D6%23cb%3Df26aa7580709a28%26origin%3Dhttp%253A%252F%252Fwww.nakedapartments.com%252Ffe786de113b5e2%26domain%3Dwww.nakedapartments.com%26relation%3Dparent%26frame%3Df39c86760c68484&amp;response_type=token%2Csigned_request%2Ccode&amp;sdk=joey"></iframe>
+            </div>
+            <div></div>
+          </div>
+          <script async="" src="http://connect.facebook.net/en_US/all.js"></script></div>
+        <script src="http://connect.facebook.net/en_US/all.js#appId=181832445204475&amp;xfbml=1"></script>
+        <fb:like font="arial" show_faces="false" width="225" layout="button_count" send="false" href="http://www.nakedapartments.com/a/profile/melissa-gomez/5ls" class=" fb_edge_widget_with_comment fb_iframe_widget"><span style="height: 21px; width: 225px;">
+          <iframe scrolling="no" id="f2d2a92b358836" name="f1b6f994681fba4" style="border: medium none; overflow: hidden; height: 21px; width: 225px;" title="Like this content on Facebook." class="fb_ltr" src="http://www.facebook.com/plugins/like.php?api_key=181832445204475&amp;channel_url=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D6%23cb%3Df1115146abed952%26origin%3Dhttp%253A%252F%252Fwww.nakedapartments.com%252Ffe786de113b5e2%26domain%3Dwww.nakedapartments.com%26relation%3Dparent.parent&amp;extended_social_context=false&amp;font=arial&amp;href=http%3A%2F%2Fwww.nakedapartments.com%2Fa%2Fprofile%2Fmelissa-gomez%2F5ls&amp;layout=button_count&amp;locale=en_US&amp;node_type=link&amp;sdk=joey&amp;send=false&amp;show_faces=false&amp;width=225"></iframe>
+          </span></fb:like>
+        </span></div>
+      <div class="clear"></div>
+    </div>
+    <div class="rightMod"> <a class="viewAll" href="/renter/listings/search?broker_id=5820">View all »</a>
+      <h2>Melissa's Apartments</h2>
+      <table id="listingSERPTable">
+        <tbody>
+          <tr data-longitude="-74.0018833" data-latitude="40.738688" data-id="750213" class="listingRow" id="listing_750213_row1">
+            <td class="border thumbnail narrow"><a href="http://www.nakedapartments.com/rental/750213-2-bedroom-w-13th-st-west-village"><img src="http://s3.amazonaws.com/nakedapartments/images/9067068/thumb.JPG?1337203251" alt="Thumb"></a></td>
+            <td class="border flag"><span class="listingTitle"><a href="http://www.nakedapartments.com/rental/750213-2-bedroom-w-13th-st-west-village">2br, West Village, $3,700</a></span>
+              <div class="newCell listingDate"> 1 day <span class="calm">on site</span> </div></td>
+          </tr>
+          <tr data-longitude="-74.0018833" data-latitude="40.738688" data-id="750214" class="listingRow" id="listing_750214_row1">
+            <td class="border thumbnail narrow"><a href="http://www.nakedapartments.com/rental/750214-2-bedroom-w-13th-st-west-village"><img src="http://s3.amazonaws.com/nakedapartments/images/9067069/thumb.JPG?1337203252" alt="Thumb"></a></td>
+            <td class="border flag"><span class="listingTitle"><a href="http://www.nakedapartments.com/rental/750214-2-bedroom-w-13th-st-west-village">2br, West Village, $3,700</a></span>
+              <div class="newCell listingDate"> 1 day <span class="calm">on site</span> </div></td>
+          </tr>
+          <tr data-longitude="-73.9787084" data-latitude="40.7366564" data-id="750638" class="listingRow" id="listing_750638_row1">
+            <td class="border thumbnail narrow"><a href="http://www.nakedapartments.com/rental/750638-studio-1st-ave-gramercy"><img src="http://s3.amazonaws.com/nakedapartments/images/9081465/thumb.JPG?1337230935" alt="Thumb"></a></td>
+            <td class="border flag"><span class="listingTitle"><a href="http://www.nakedapartments.com/rental/750638-studio-1st-ave-gramercy">Studio, Gramercy, $2,850</a></span>
+              <div class="newCell listingDate"> about 23 hours <span class="calm">on site</span> </div></td>
+          </tr>
+          <tr data-longitude="-73.978095" data-latitude="40.765807" data-id="750644" class="listingRow" id="listing_750644_row1">
+            <td class="border thumbnail narrow"><a href="http://www.nakedapartments.com/rental/750644-1-bedroom-w-58th-st-clinton"><img src="http://s3.amazonaws.com/nakedapartments/images/9081501/thumb.JPG?1337230983" alt="Thumb"></a></td>
+            <td class="border flag"><span class="listingTitle"><a href="http://www.nakedapartments.com/rental/750644-1-bedroom-w-58th-st-clinton">1br, Clinton, $3,650</a></span>
+              <div class="newCell listingDate"> about 23 hours <span class="calm">on site</span> </div></td>
+          </tr>
+          <tr data-longitude="-73.978454" data-latitude="40.7656744" data-id="750635" class="listingRow" id="listing_750635_row1">
+            <td class="border thumbnail narrow"><a href="http://www.nakedapartments.com/rental/750635-1-bedroom-w-58th-st-midtown-center"><img src="http://s3.amazonaws.com/nakedapartments/images/9081460/thumb.JPG?1337230931" alt="Thumb"></a></td>
+            <td class="border flag"><span class="listingTitle"><a href="http://www.nakedapartments.com/rental/750635-1-bedroom-w-58th-st-midtown-center">1br, Midtown Center, $3,100</a></span>
+              <div class="newCell listingDate"> about 23 hours <span class="calm">on site</span> </div></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="rightMod">
+      <h2>Melissa's Specialties</h2>
+      <div class="unitContainer">
+        <h3>Manhattan</h3>
+        Chelsea, Clinton, East Village, Flatiron, Greenwich Village, Hell's Kitchen, Lower East Side, Midtown, Midtown East, Upper East Side, Upper West Side, West Village </div>
+    </div>
+  </div>
+  <!-- End: Right Col --> 
+  
+</div>
