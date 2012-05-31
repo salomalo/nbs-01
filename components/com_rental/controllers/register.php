8d7bcc388c1f3ccdce0d58f40e10af98b8b50fd6
nguyenxuanmui <nguyenxuanmui@gmail.com>
2012-05-31 09:22:27 +0000

    update view register

--- components/com_rental/controllers/register.php
+++ components/com_rental/controllers/register.php
@@ -4,7 +4,26 @@ class RentalControllerRegister extends JController
 {
 	function renter()
 	{
-		echo "$('#new_renter').replaceWith('test'); ";
-		exit();
+		// Get the document object.
+		$document =& JFactory::getDocument();
+		
+		// Set the MIME type for JSON output.
+		$document->setMimeEncoding('text/javascript');
+		
+		header('Content-Type: text/javascript; charset=' . $document->getCharset());
+		
+		$contents = "hide_form_errors();
+					 hide_all_steps();
+					 $('#signup_step_2').show();
+					 $('#step').val('3');
+					 na.renter_step_signup.indicator(2);";
+		
+		$contents = str_replace('/', '\/', preg_replace('/[\r\n]+/', null, $contents));
+		$contents = str_replace('"', '\"', $contents);
+		$contents = str_replace("'", "\'", $contents);
+		
+		echo $contents;
+		
+		jexit();
 	}
 }
