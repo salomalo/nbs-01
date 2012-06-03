a0c52aa1470421e3b7f7f7511611e0be2b19b9e6
nguyenxuanmui@gmail.com <nguyenxuanmui@gmail.com@067289a7-69ab-d125-2915-3cd7ab555523>
2012-06-03 23:58:48 +0000

    Update sql

--- trunk/SQL/Update.sql
+++ trunk/SQL/Update.sql
@@ -26,3 +26,22 @@ ALTER TABLE  `jos_retal_agents` ADD  `bio` TEXT NOT NULL AFTER  `country`;
 -- 30/05/2012 - thaiht - alter table agent
 ALTER TABLE  `jos_rental_apartments` ADD  `latitude` VARCHAR(200) NOT NULL AFTER  `country`;
 ALTER TABLE  `jos_rental_apartments` ADD  `longitude` VARCHAR(200) NOT NULL AFTER  `country`;
+
+-- 04/06/2012 - muinx - add table renter
+DROP TABLE IF EXISTS `jos_rental_renters`;
+CREATE TABLE IF NOT EXISTS `jos_rental_renters` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `apartment_size` text,
+  `neighborhood_ids` text,
+  `max_rent` float(11,2) DEFAULT NULL,
+  `have_a_pet` tinyint(1) DEFAULT NULL,
+  `roommate` tinyint(1) DEFAULT NULL,
+  `email_alert` varchar(255) DEFAULT NULL,
+  `financial_info` text,
+  `more_info` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
